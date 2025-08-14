390c503b3177cb7fad9e047b2364a84907528472
L-a-z-e <yysi8771@gmail.com>
2025-08-14 23:19:31 +0900

    scheduler

--- flow-control/src/main/java/com/laze/flowcontrol/FlowControlApplication.java
+++ flow-control/src/main/java/com/laze/flowcontrol/FlowControlApplication.java
@@ -3,9 +3,11 @@ package com.laze.flowcontrol;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableScheduling
 public class FlowControlApplication {
 
 	public static void main(String[] args) {
--- flow-control/src/main/java/com/laze/flowcontrol/service/UserQueueService.java
+++ flow-control/src/main/java/com/laze/flowcontrol/service/UserQueueService.java
@@ -1,21 +1,31 @@
 package com.laze.flowcontrol.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
 import static com.laze.flowcontrol.exception.ErrorCode.QUEUE_ALREADY_REGISTERED_USER;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
 
     private final ReactiveRedisTemplate<String, String> redisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
     // 대기열 등록 API
     public Mono<Long> registerWaitingQueue(final String queue, final Long userId) {
@@ -49,5 +59,27 @@ public class UserQueueService {
                 .rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 3000)
+    public void scheduleAllowUser() {
+        log.info("called scheduled allowUser");
+
+        if (!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
+
+        var maxAllowUserCount = 3L;
+
+        redisTemplate.scan(ScanOptions.scanOptions()
+                .match(USER_QUEUE_WAIT_KEY_SCAN)
+                .count(100)
+                .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+                .subscribe();
+
     }
 }
